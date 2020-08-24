1d8e17d6a440bb8d2c2965a45fcfca22e515e742
denise-n <denise.ndalamba@gmail.com>
2020-08-24 12:17:36 -0400

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <div id="app">
-    <SimpleList />
+  <div class="mx-auto p-6 bg-gray-200">
+    <CardList />
   </div>
 </template>
 
 <script>
-import SimpleList from './components/SimpleList.vue'
+import CardList from './components/CardList.vue'
 
 export default {
   name: 'App',
   components: {
-    SimpleList
+    CardList
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4-gap-4">
+      <div
+        v-for="person in people"
+        :key="person.id"
+        class="bg-white rounded-lg p-6 shadow"
+      >
+        <img
+          :src="person.avatar"
+          :alt="person.name"
+          class="h-16 w-16 rounded-full mx-auto"
+        />
+        <div class="text-center">
+          <h2 class="text-lg">
+            {{ person.name }}
+          </h2>
+          <div class="text-purple-500">{{ person.department }}</div>
+          <div class="text-gray-600 text-sm">{{ person.email }}</div>
+          <div class="text-gray-600 text-sm">{{ person.phone }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      people: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/people')
+      .then(response => {
+        this.people = response.data
+      })
+      .catch(err => {
+        console.log('There was an error:', err.response)
+      })
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
